950139ea49b2fb51330e36c4073f1f8f3005a50b
Dunya Kirkali <dunyakirkali@gmail.com>
2015-03-28 13:31:19 +0100

    getting there

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.1.2'
 
-gem 'paypal-sdk-adaptivepayments'
+gem 'pp-adaptive'
 gem 'launchy'
 gem 'pry'
 gem 'colorize'
--- delayed_payments.rb
+++ delayed_payments.rb
@@ -1,7 +1,8 @@
-require 'paypal-sdk-adaptivepayments'
+require 'pp-adaptive'
 require 'pry'
 require 'launchy'
 require 'colorize'
+require 'yaml'
 
 def run
   pry.binding
@@ -10,81 +11,57 @@ end
 # Setup the Payment and return pay object
 def setup_payment
   receiver = { email: 'tr-personal@gmail.com' }
-  api.build_pay payment_options(receiver)
+  api.execute :Pay, payment_options(receiver)
 end
 
 # Redirect the Customer to PayPal for Authorization and return response
-def redirect_to_paypal(pay)
-  response = api.pay(pay)
-  if response.success? && response.payment_exec_status != 'ERROR'
-    p 'success'.green
+def redirect_to_paypal(response)
+  if response.success?
+    p "Pay key: #{response.pay_key}".green
+    Launchy.open(api.payment_url(response))
   else
-    p response.error[0].message.red
+    p "#{response.ack_code}: #{response.error_message}".red
   end
-  response
 end
 
 # Retrieve Data about the Payment (Optional)
-def retrieve_payment_data
+def retrieve_payment_data(pay_key)
+  api.execute(:PaymentDetails, pay_key: pay_key) do |response|
+    if response.success?
+      p "Payment status: #{response.payment_exec_status}".green
+    else
+      p "#{response.ack_code}: #{response.error_message}".red
+    end
+    return response
+  end
 end
 
 # Make a Payment to One or More Secondary Receivers
 def make_payment_to_secondary(pay_key)
-  api.build_pay secondary_payment_options(receiver)
+  api.pay pay_key
 end
 
 def payment_options(receiver)
   {
-    actionType: 'PAY_PRIMARY',
-    currencyCode: 'USD',
-    feesPayer: 'EACHRECEIVER',
-    receiverList: {
-      receiver: [
-        {
-          amount:    25.00,
-          email:     receiver[:email],
-          primary:   false
-        },
-        {
-          amount:    30.00,
-          email:     'opotto@gmail.com',
-          primary:   true
-        }
-      ]
-    },
-    returnUrl: 'http://www.yourdomain.com/success.html',
-    cancelUrl: 'http://www.yourdomain.com/cancel.html'
-  }
-end
-
-
-def secondary_payment_options(receiver)
-  {
-    actionType: 'PAY_PRIMARY',
-    currencyCode: 'USD',
-    feesPayer: 'EACHRECEIVER',
-    receiverList: {
-      receiver: [
-        {
-          amount:    25.00,
-          email:     receiver[:email],
-          primary:   false
-        },
-        {
-          amount:    30.00,
-          email:     'opotto@gmail.com',
-          primary:   true
-        }
-      ]
-    },
-    returnUrl: 'http://www.yourdomain.com/success.html',
-    cancelUrl: 'http://www.yourdomain.com/cancel.html'
+    action_type:    "PAY",
+    currency_code:  "USD",
+    cancel_url:     "https://your-site.com/cancel",
+    return_url:     "https://your-site.com/return",
+    receivers:      [
+      { email: "opotto@gmail.com", amount: 100, primary: true },
+      { email: receiver[:email], amount: 90 }
+    ]
   }
 end
 
 def api
-  PayPal::SDK.load('paypal.yml', 'development')
-  PayPal::SDK::AdaptivePayments::API.new
+  AdaptivePayments::Client.new(
+    sandbox: true,
+    app_id: 'APP-80W284485P519543T',
+    user_id: 'opotto_api1.gmail.com',
+    password: '5X4XVKF7XUV3TG9J',
+    signature: 'AH6d1xBXrGOpE-RZEFdL.zYTFUW0ANeJL59zIFyiDm3ijpwFopvxNwW9'
+  )
 end
 
 run
