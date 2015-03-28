fa3611cc8cd3f8758396d3427259f2ccaa3e0d7f
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-28 12:55:21 +0200

    added setup_payment method functions

--- delayed_payments.rb
+++ delayed_payments.rb
@@ -7,7 +7,11 @@ end
 
 # Set Up the Payment
 def setup_payment
-
+  PayPal::SDK.load('paypal.yml',  ENV['RACK_ENV'] || 'development')
+  api ||= PayPal::SDK::AdaptivePayments::API.new
+  pay = api.build_pay(payment_options)
+  response = api.pay(pay)
+  response.success? && response.payment_exec_status != 'ERROR'
 end
 
 # Redirect the Customer to PayPal for Authorization
@@ -23,4 +27,31 @@ end
 # Make a Payment to One or More Secondary Receivers
 def make_payment_to_secondary
 
+end
+
+private
+
+def payment_options
+  {
+    actionType: PAY_PRIMARY
+    currencyCode: USD
+    feesPayer: EACHRECEIVER
+    receiverList: {
+      receiver: [
+        {
+          amount:    25.00,
+          email:     'onurkucukkece-buyer@gmail.com',
+          primary:   false
+        },
+        {
+          amount:    25.00 * 0.01,
+          email:     'dunyakirkali-buyer@yahoo.fr',
+          primary:   true
+        }
+      ]
+    }
+    requestEnvelope: errorLanguage=en_US
+    returnUrl: 'http://www.yourdomain.com/success.html'
+    cancelUrl: 'http://www.yourdomain.com/cancel.html'
+  }
 end
