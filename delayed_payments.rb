48a7c83685d6334cc05f9daad00d15560fdf888b
Onur Küçükkeçe <onurkucukkece@gmail.com>
2015-03-28 12:32:08 +0200

    added 4 empty functions for paypal payments

---
+++ delayed_payments.rb
@@ -0,0 +1,19 @@
+# Set Up the Payment
+def setup_payment
+
+end
+
+# Redirect the Customer to PayPal for Authorization
+def redirect_to_paypal
+
+end
+
+# Retrieve Data about the Payment (Optional)
+def retrieve_payment_data
+
+end
+
+# Make a Payment to One or More Secondary Receivers
+def make_payment_to_secondary
+
+end
